fb59b5df33e3b443c6c96079e825a404ad0a79fc
vagrant <vagrant@precise32.(none)>
2014-02-21 06:33:30 +0000

    Consistency and improved interaction for many pages, nothing changed to prescriptions

--- routes/history.js
+++ routes/history.js
@@ -1,4 +1,15 @@
 // Get all of our friend data
+
+var models = require('../models');
+
 exports.view = function(req, res){
-	res.render('history');
+	models.UserInfo
+		.find({"UserName": req.session.username})
+		.exec(renderName);
+
+function renderName(err,userData){
+ 	if(err) console.log(err);
+ 	console.log(userData);
+	res.render('history',userData[0]);
+}
 };
