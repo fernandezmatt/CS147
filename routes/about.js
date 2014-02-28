8ed0db6da53dfcf9716749f81dd5f54c60b4db48
vagrant <vagrant@precise32.(none)>
2014-02-28 07:19:24 +0000

    about commit

---
+++ routes/about.js
@@ -0,0 +1,15 @@
+// Get all of our friend data
+
+var models = require('../models');
+
+exports.view = function(req, res){
+	models.UserInfo
+		.find({"UserName": req.session.username})
+		.exec(renderName);
+
+function renderName(err,userData){
+ 	if(err) console.log(err);
+ 	console.log(userData);
+	res.render('about',userData[0]);
+}
+};
